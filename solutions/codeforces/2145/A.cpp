97ffe612a1e3cb805ac4693d397d481f211acbfd
AndriaBeridze <andria24b@gmail.com>
2025-10-06 22:44:11 -0400

    Solve codeforces/2145/A

---
+++ solutions/codeforces/2145/A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+    
+using namespace std;
+int TC = 0;
+    
+#define IOS ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+#define int long long
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define size(v) (int) v.size()
+#define left node * 2, l, (l + r) / 2
+#define right node * 2 + 1, (l + r) / 2 + 1, r
+#define inf (int) 1e18
+    
+void solve() {
+    int n;
+    cin >> n;
+    cout << (3 - n % 3) % 3 << endl;
+}
+    
+signed main(){
+    int q = 1;
+    cin >> q;
+    while(++TC <= q) {
+        solve();
+    }
+}
